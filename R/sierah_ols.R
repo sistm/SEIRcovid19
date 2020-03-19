455ce4408a2f7b2e9cf31f4cc8ae6f1961a13d4b
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-19 19:04:53 +0100

    better seirah_ols

--- R/sierah_ols.R
+++ R/sierah_ols.R
@@ -3,29 +3,55 @@
 #' @import deSolve
 #'
 #' @examples
-#' dataFR<-get_data_covid19(maille = "France",
-#'              source = "Santé publique France")
-#' #Perform OLS            
-#' optimresult<-optim(c(log(1.75),log(0.41)), ols_sierah,data=dataFR)
-#' #Plot fitted vs. observed 
-#' ols_sierah(optimresult$par,data=dataFR,plot.comparison = TRUE)
+#'
+#' data_FRA <- get_data_covid19(maille_cd = "FRA",
+#'                            source_ch = "sante-publique-france")
+#' #Perform OLS
+#' optimresult_FRA <- optimx(c(log(1.75), log(0.41)),
+#'                       fn = seirah_ols, data=data_FRA,
+#'                       method = "Nelder-Mead")
+#' #Plot fitted vs. observed
+#' fit_FRA <- seirah_ols(b = c(optimresult_FRA$p1, optimresult_FRA$p2),
+#'                   data = data_FRA, fit = TRUE)
+#' plot(fit_FRA)
 #'
 #'
-#'GrandEST<-get_data_covid19(maille = "Grand-Est",
-#'               source = "ARS Grand-Est")
-#'optimresult<-optim(c(log(1.75),log(0.41)), ols_sierah,data=GrandEST,popSize=5518000 ,dailyMove=0.1*5518000)
-#'ols_sierah(optimresult$par,data=GrandEST,popSize=5518000 ,dailyMove=0.1*5518000,plot.comparison = TRUE)
+#'data_GE <- get_data_covid19(maille_cd = "REG-44",
+#'                            source_ch = "agences-regionales-sante")
+#'#optimresult_GE <- optim(par = c(log(1.75),log(0.41)), fn = seirah_ols,
+#'                        data = data_GE, popSize=5518000 ,dailyMove=0.1*5518000)
+#'#plot(seirah_ols(optimresult_GE$par, data=data_GE, popSize=5518000,dailyMove=0.1*5518000, fit = TRUE))
+#'optimxresult_GE <- optimx(c(log(1.75),log(0.41)),
+#'                      fn = seirah_ols, data=data_GE, popSize=5518000 ,dailyMove=0.1*5518000,
+#'                      method = "Nelder-Mead")
+#'fit_GE <- seirah_ols(b = c(optimxresult_GE$p1, optimxresult_GE$p2),
+#'                   data=data_GE, popSize=5518000 ,dailyMove=0.1*5518000, fit = TRUE)
+#'plot(fit_GE)
 #'
 #'
-#'NouvelleAquitaine<-get_data_covid19(maille = "Nouvelle-Aquitaine",
-#'               source = "ARS Nouvelle-Aquitaine")
-#'NouvelleAquitaine<-NouvelleAquitaine[41:54,]
-#'NouvelleAquitaine$day<-NouvelleAquitaine$day-40
-#'optimresult<-optim(c(log(1.75),log(0.41)), ols_sierah,data=NouvelleAquitaine,popSize=5987000 ,dailyMove=0.1*5987000)
-#'ols_sierah(optimresult$par,data=NouvelleAquitaine,plot.comparison = TRUE,popSize=5987000 ,dailyMove=0.1*5987000)
+#'
+#'
+#'data_NA <- get_data_covid19(maille_cd = "REG-75",
+#'                            source_ch = "agences-regionales-sante")
+#'data_NA <- data_NA[-c(1:40), ]
+#'data_NA$day <- data_NA$day-40
+#'
+#'#optimresult_NA<-optim(c(log(1.75),log(0.41)), seirah_ols, data=data_NA, popSize=5987000 ,dailyMove=0.1*5987000)
+#'#plot(seirah_ols(optimresult$par,data=data_NA, fit = TRUE, popSize=5987000 ,dailyMove=0.1*5987000))
+#'
+#'optimxresult_NA <- optimx(c(log(1.75), log(0.41)),
+#'                         fn = seirah_ols, data=data_NA, popSize=5987000 ,dailyMove=0.1*5987000,
+#'                         method = "Nelder-Mead")
+#' #Plot fitted vs. observed
+#' fit_FRA <- seirah_ols(b = c(optimxresult_NA$p1, optimxresult_NA$p2),
+#'                       data = data_NA, popSize=5987000 ,dailyMove=0.1*5987000, fit=TRUE)
+#' plot(fit_FRA)
+#'
 #' @export
-sierah_ols <- function(b,data,alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,popSize=65000000,dailyMove=1000000,
-                       verbose = TRUE, plot_comparison=FALSE){
+seirah_ols <- function(b, data,
+                       alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,
+                       popSize=65000000,dailyMove=1000000,
+                       verbose = TRUE, fit=FALSE){
 
   transmission <- exp(b[1])
   ascertainment <- exp(b[2])
@@ -34,7 +60,6 @@ sierah_ols <- function(b,data,alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,popSize=65000000
     message("transmission:", transmission)
     message("ascertainment:", ascertainment)
   }
-
 
   # Initialization
   E0 <- data[1, "cas_confirmes_incident"]*2 # Twice the number of cases (346 ref)
@@ -56,21 +81,39 @@ sierah_ols <- function(b,data,alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,popSize=65000000
   #  plot(sol)
   #}
 
-  selectedsol <- sol[which(sol[,"time"]%in%data$day), 4]
-  res <- sum( (data[, "cas_confirmes_incident"] - selectedsol) **2) /
+  selectedsol <- sol[which(sol[,"time"] %in% data$day), "I"]
+  ss <- sum( (data[, "cas_confirmes_incident"] - selectedsol) **2) /
     length(data[, "cas_confirmes_incident"])
 
   if(verbose){
-    message("Objective function:", res)
-  }
-  if(plot_comparison){
-    ylimmax<-max(rbind(selectedsol,data[,"cas_confirmes_incident"]))+1
-    plot(sol[which(sol[,"time"]%in%data$day),1],selectedsol,type="l",col="blue",ylim=c(0,ylimmax),ylab="Proportion")
-    points(data[,"day"], data[,"cas_confirmes_incident"],col="black")
+    message("Sum of  function:", ss)
   }
 
-  class(res) <- "seirah_estim"
+  if(fit){
+    res <- list("solution" = sol,
+                "data" = data)
+    class(res) <- "seirah_estim"
+  }else{
+    res <- ss
+  }
   return(res)
 }
 
 
+#' Plotting method for an SEIRAH OLS fit object
+#'
+#' @import ggplot2
+#'
+#' @export
+plot.seirah_estim <- function(x){
+
+  sol_obstime <- x$solution[which(x$solution[,"time"] %in% x$data$day), ]
+  ylimmax <- max(rbind(sol_obstime[,"I"], x$data[,"cas_confirmes_incident"]))+1
+  data2plot <- cbind.data.frame(x$data, sol_obstime)
+  ggplot(data2plot, aes(x=date)) +
+    geom_point(aes(y = cas_confirmes_incident, color = "Observed")) +
+    geom_line(aes(y = I, color = "SEIRAH")) +
+    theme_classic() +
+    ylab("Number of incident cases") +
+    scale_color_manual("", values=c("black", "blue"))
+}
