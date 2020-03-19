64bade3d8a5bd22c3cd25f02129ec198f0c831d6
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-19 20:19:51 +0100

    cleaner all regions

--- All_regions_fit.R
+++ All_regions_fit.R
@@ -1,6 +1,9 @@
+rm(list=ls())
+
 region_data <- get_data_covid19_bylevel()
 region_pop <- read.delim("data/population_regionsFR_INED1erjan2020.tsv")
 
+library(dplyr)
 res <- list()
 for (r in names(region_data)){
   pop_reg <- region_pop %>%
@@ -19,7 +22,6 @@ all_plots <- list()
 for (r in names(region_data)){
   all_plots[[r]] <- plot(res[[r]]) + ggtitle(r)
 }
-
 
 library(cowplot)
 cowplot::plot_grid(plotlist = all_plots, ncol=3)
