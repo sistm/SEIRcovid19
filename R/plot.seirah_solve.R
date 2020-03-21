c1663d490d7f790fa9b429eed4e9ac22cc25a36e
mprague <melanie.prague@inria.fr>
2020-03-21 16:52:55 +0100

    Add presentation

--- R/plot.seirah_solve.R
+++ R/plot.seirah_solve.R
@@ -3,19 +3,16 @@
 #'@param x an object of class \code{seirah_solve}
 #'
 #'@export
-plot.seirah_solve <- function(x, locator.legend=F){
+plot.seirah_solve <- function(x){
 
-  ylimmax <- max(rbind(x[,4], x[,5], x[,6], x[,7])) + 1
-
-  plot(x[, "time"], x[, "I"],type="l",col="blue",ylim=c(0,ylimmax),ylab="Proportion",xlab="Time")
-  lines(x[, "time"], x[, "A"],col="green")
-  lines(x[, "time"], x[, "H"],col="orange")
-  lines(x[, "time"], x[, "R"],col="red")
-
-  if(locator.legend){
-    legend(locator(1), legend=c("Infectious","Not-reported","Hospitalized","Recovered"), col=c("blue","orange","red","green"), lty=1, cex=0.8)
-  }else{
-    legend(1, 0.9*ylimmax, legend=c("Infectious","Not-reported","Hospitalized","Recovered"), col=c("blue","orange","red","green"), lty=1, cex=0.8)
-  }
+  par(mfrow=c(2,3))
+  plot(x[,"time"],x[,"S"],type="l",xlab="Time",ylab="S",col="#56B4E9")
+  plot(x[,"time"],x[,"E"],type="l",xlab="Time",ylab="E",col="#F0E442")
+  plot(x[,"time"],x[,"I"],type="l",xlab="Time",ylab="I",col="#E69F00")
+  plot(x[,"time"],x[,"R"],type="l",xlab="Time",ylab="R",col="#009E73")
+  plot(x[,"time"],x[,"A"],type="l",xlab="Time",ylab="A",col="#CC79A7")
+  plot(x[,"time"],x[,"H"],type="l",xlab="Time",ylab="H",col="#D55E00")
+  abline(h=chiffres[which(chiffres$goodid==as.character(indivParams[i,1])),"ICUnb"],col="red")
+  par(mfrow=c(1,1))
 
 }
--- R/seirah_estim.R
+++ R/seirah_estim.R
@@ -8,9 +8,13 @@
 #' data_FRA <- get_data_covid19(maille_cd = "FRA",
 #'                              source_ch = "sante-publique-france")
 #' fit_FRA <- seirah_estim(binit = c(1.75, 0.41),
-#'                         data = data_FRA)
+#'                         data = data_FRA,dailyMove=0.01*popSize)
 #' plot(fit_FRA)
-#'
+#' plot.seirah_solve(fit_FRA$solution)
+#' fit_FRA <- seirah_estim(binit = c(fit_FRA$parameters$transmission  , fit_FRA$parameters$ascertainment  ),
+#'                         data = data_FRA,dailyMove=0.1*popSize,timeconf=15,newdailyMove=0,factorreductrans=2,optim_ols=FALSE)
+#' plot.seirah_solve(fit_FRA$solution)                        
+#' 
 #' data_GE <- get_data_covid19(maille_cd = "REG-44",
 #'                             source_ch = "agences-regionales-sante")
 #' fit_GE <- seirah_estim(binit = c(1.75, 0.41),
@@ -28,26 +32,6 @@
 #' simul_xihong<- seirah_estim(binit=c(1.75, 0.41),data=NULL,stateinit=c(9999467,346,80,0,80,27),
 #'                             initwithdata=FALSE,alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,popSize=10000000,dailyMove=500000,verbose = TRUE,optim_ols = FALSE)
 #' plot_list.seirah_estim(list(simul_xihong))
-
-
-
-binit=as.numeric(indivParams[i,2:3])
-data=data[which(data$goodID==as.character(indivParams[i,1])),]
-alpha=1
-De=5.2
-Di=2.3
-Dq=10
-Dh=30
-popSize=chiffres[which(chiffres$goodid==as.character(indivParams[i,1])),"size"]
-dailyMove=0.01*chiffres[which(chiffres$goodid==as.character(indivParams[i,1])),"size"]
-verbose = TRUE
-optim_ols=FALSE
-timeconf=14
-newdailyMove=0.00001
-factorreductrans=3
-stateinit=NULL
-initwithdata=TRUE
-
 seirah_estim <- function(binit, data=NULL,stateinit=NULL,initwithdata=TRUE,
                          alpha=1,De=5.2,Di=2.3,Dq=10,Dh=30,
                          popSize=65000000, dailyMove=0.1*popSize,
