3df6839df50cbb515a8f194de5bad908a3a27f9b
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-21 16:08:44 +0100

    better popreg doc

--- R/popreg.R
+++ R/popreg.R
@@ -12,7 +12,11 @@
 #'
 #'@examples
 #'data(popreg)
+#'#popreg <- read.delim("data/raw/population_regionsFR_INED1erjan2020.tsv",
+#'                      encoding = "UTF8")
 #'
-#'@source INED
+#'@source INED \url{https://www.ined.fr/fichier/s_rubrique/159/estim.pop.nreg.sexe.gca.1975.2020.fr.xls}
+#'stored in \code{'data/raw/population_regionsFR_INED1erjan2020.tsv'}
 #'
 #'
+"popreg"
