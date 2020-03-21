696bc91c199810353bb32807e75d69f30bfd8556
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-21 15:35:34 +0100

    adding popreg as data

---
+++ R/popreg.R
@@ -0,0 +1,18 @@
+#'Population numbers in France
+#'
+#'A dataset containing the populations of France as well as broken down by region
+#'as of 2020-01-01 (according to INED)
+#'
+#' @format A data frame with 19 rows and 3 variables:
+#' \describe{
+#'   \item{maille_nom}{name in French}
+#'   \item{maille_code}{administrative code}
+#'   \item{population}{population}
+#' }
+#'
+#'@examples
+#'data(popreg)
+#'
+#'@source INED
+#'
+#'
