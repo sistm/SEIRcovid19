20e98cff0101e8137254898ccc5d7e508981f06c
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-25 21:31:58 +0100

    DREES 2018 ICU data

---
+++ R/ICUcapacity_FR.R
@@ -0,0 +1,17 @@
+#'Numbers of ICU beds in France
+#'
+#'A dataset containing the numbers of ICU beds in France and in each region
+#'according to DREES as of 2018-12-31
+#'
+#' @format A data frame with 19 rows and 2 variables
+#'
+#'@examples
+#'data(ICUcapacity_FR)
+#'#ICUcapacity_FR <- read.delim("data/raw/capacite_rea_regions_saediffusiondrees.txt")
+# #save(ICUcapacity_FR, file="data/ICUcapacity_FR.RData")
+#'
+#'@source DREEES 2018 \url{https://www.sae-diffusion.sante.gouv.fr/sae-diffusion/recherche.htm}
+#'obtaine on 2020-03-25 and stored in
+#'\code{'data/raw/capacite_rea_regions_saediffusiondrees.txt'}
+#'
+"ICUcapacity_FR"
