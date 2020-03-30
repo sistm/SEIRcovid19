4e59304851f8945ce3a71dfd1dfe4a9950a2c744
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-03-30 18:53:34 +0200

    data 4 monolix from SPF + SURSAUD

---
+++ R/prepare_data_4monolix.R
@@ -0,0 +1,40 @@
+#' Prepare data from Santé Publique France and Sursaud for monolix
+#'
+#' @import reshape2 dplyr
+#' @export
+#'
+#' @examples
+#' SPF_I_SURSAUD_H_covid19fr <- prepare_data_4monolix()
+#' save(SPF_I_SURSAUD_H_covid19fr, file = "SPF_I_SURSAUD_H_covid19fr.RData")
+#'
+prepare_data_4monolix <- function( update_from_source = TRUE){
+  all <- get_data_covid19_bylevel(level = "region", source3 = "SPF", update_from_source = TRUE)
+
+  prep1reg <- function(x){
+  temp <- x %>%
+    mutate(reg_id = maille_code, I = cas_confirmes_incident, H = hospitalisation_incident) %>%
+    select(reg_id, date, day, I, H)
+
+  I0 <- temp %>% top_n(-1, date) %>% pull(I)
+  H0 <- temp %>% top_n(-1, date) %>% pull(H)
+  if(is.na(I0) | I0 == 0){
+    I0 <- 1
+  }
+  if(is.na(H0)){
+    H0 <- 0
+  }
+
+  x_final <- temp %>%
+    mutate(init_I0 = I0, init_H0 = H0) %>%
+    reshape2::melt(id.vars=c("reg_id", "date", "day", "init_I0", "init_H0"), variable.name="obs_id",
+                   value.name = "obs") %>%
+    mutate(isolation = 1*(! date < "2020-03-18"))
+  }
+
+  all_prep <- lapply(all, prep1reg)
+
+  all_df <- do.call(rbind, all_prep)
+  rownames(all_df) <- NULL
+
+  return(all_df)
+}
