f239f7abc87b28f6273780e24e414a3c6e0db02f
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-04-29 18:25:32 +0200

    better error/message

--- R/SetSpecificInit.R
+++ R/SetSpecificInit.R
@@ -10,19 +10,18 @@ SetSpecificInit <- function(obj, isspecific)
 
 SetSpecificInit.default <- function(obj, isspecific)
 {
-  print("No method implemented for this class")
+  error("No method implemented for this class")
   return(obj)
 }
 
 #' @describeIn Set IsSpecificInit attribute for an object of class \code{OdeSystem}
-
 SetSpecificInit.OdeSystem <- function(ode, isspecific)
 {
   if (length(isspecific)==length(ode$IsSpecificInit)){
-    print("Set all Specific Init info")
+    message("Set all Specific Init info")
     ode$IsSpecificInit<-isspecific
   }else{
-    print("Set some Specific Init info")
+    message("Set some Specific Init info")
     for (p in 1:length(isspecific)){
       ode$IsSpecificInit[names(ode$InitState)==names(isspecific[p])]<-isspecific[[p]]
     }
