ce21fd0aa933fe68b0d070b17f7b7394eef3c53b
melanieprague <melanie.prague@inria.fr>
2020-07-09 14:13:20 +0200

    New model

--- MonolixFile/SEIDRAH.R
+++ MonolixFile/SEIDRAH.R
@@ -2,7 +2,7 @@ devtools::load_all('.')
 Sys.setlocale("LC_NUMERIC","C")
 library(xtable)
 
-nameproject<-"fourstepEAKalman_small"
+nameproject<-"SEIRAH"
 ModelFile<-paste('./MonolixFile/','Estim_',nameproject,".txt",sep="")
 ModeFilename<-paste("Simul_",nameproject,".txt",sep="")
 
@@ -25,6 +25,10 @@ param<-c(b1=1,
          betaw2=1,
          betaw3=1,
          betaw4=1,
+         betawp1=1,
+         betawp2=1,
+         betawp3=1,
+         betawp4=1,
          rsent=0.0337,
          alpha=0.55,
          De=5.1,
@@ -33,8 +37,7 @@ param<-c(b1=1,
          Dh=30,
          pct=1,
          popsize=-100,
-         timeSinceConf=-100,
-         initIref=-100)
+         timeSinceConf=-100)
 # Define all init state of the ODE systme with value, it should be initVar
 init  <- c(initS=0, 
            initE=1,
@@ -50,7 +53,7 @@ ode_def<-seirah_ode
 myOde<-OdeSystem(func=ode_def,param=param,init=init,modname = model_name)
 myOde
 # Définition spécifique pour les états initiaux => A et S
-SpecificInit<-list(initA=1,initS=1,initI=1)
+SpecificInit<-list(initA=1,initS=1,initI=1,initH=1)
 myOde<-SetSpecificInit(myOde,SpecificInit)
 # Variability => 0 = fixed, 1 mobile sans effet aléatoire, 2 mobile avec effet aléatoire (ajout d'un beta)
 ## Variability has to be defined for Parameter and InitState
@@ -60,6 +63,10 @@ ParamVar<-list(b1=1,
                betaw2=2,
                betaw3=2,
                betaw4=2,
+               betawp1=2,
+               betawp2=2,
+               betawp3=2,
+               betawp4=2,
                Dq=1)
 myOde<-SetParamVariability(myOde,ParamVar)
 # Param
@@ -68,27 +75,29 @@ myOde<-SetInitVariability(myOde,InitVar)
 # Distribution of the parameter, default is logNormal
 # Distribution as to be define for Parameter and IniState
 #Param
-Paramdist<-list(betaw1="normal",betaw2="normal",betaw3="normal",betaw4="normal")
+Paramdist<-list(betaw1="normal",betaw2="normal",betaw3="normal",betaw4="normal",betawp1="normal",betawp2="normal",betawp3="normal",betawp4="normal")
 myOde<-SetParamDistribution(myOde,Paramdist)
 
-ParamRegressor<-list(popsize=1,timeSinceConf=1,initIref=1)
+ParamRegressor<-list(popsize=1,timeSinceConf=1,timeSinceDeConf=1)
 myOde<-SetParamIsRegressor(myOde,ParamRegressor)
 # Init
-InitRegressor<-list(initH=1)
-myOde<-SetInitIsRegressor(myOde,InitRegressor)
+#InitRegressor<-list(initH=1)
+#myOde<-SetInitIsRegressor(myOde,InitRegressor)
 # Data input info
-path<-"./MonolixFile/DATABIOMKALMAN.txt"
+path<-"./MonolixFile/SEIRAH_20200709.txt"
 sep<-"\t"
-header<-c("ignore","id","ignore","observation","regressor","ignore","time","ignore","regressor","ignore","ignore","regressor","regressor","ignore","obsid")
+header<-c("ignore","id","ignore","observation","regressor","ignore","time","ignore","regressor","regressor","ignore","obsid","ignore")
 
 myOde<-SetDataInput(myOde,path,header,sep)
 
-SpecificInitBloc<-c("I_0=initIref/pct",
+SpecificInitBloc<-c("I_0=E_0/10",
+                    "H_0=E_0/10/5",
                     "A_0=I_0*(1-rsent)/rsent",
                     "S_0=popsize-E_0-I_0-R_0-A_0-H_0")
 ModelStatBloc<-c("if (timeSinceConf==0)",
                  "current = 0",
                  "else",
+                 "if (timeSinceDeConf==0)",
                  "if (timeSinceConf<7)",
                  "current = betaw1",
                  " end",
@@ -100,6 +109,20 @@ ModelStatBloc<-c("if (timeSinceConf==0)",
                  "end",
                  "if (timeSinceConf>=21) ",
                  "current = betaw4",
+                 "end",
+                 "else",
+                 "if (timeSinceDeConf<15)",
+                 "current = betawp1",
+                 " end",
+                 "if (timeSinceDeConf>=15) & (timeSinceDeConf<30)",
+                 "current = betawp2",
+                 "end",
+                 "if (timeSinceDeConf>=30) & (timeSinceDeConf<45)",
+                 "current = betawp3",
+                 "end",
+                 "if (timeSinceDeConf>=45) ",
+                 "current = betawp4",
+                 "end",
                  "end",
                  "end",
                  "transmission=b1*exp(current)",
@@ -124,7 +147,7 @@ PopInitValue=c(b1=1.95,
                initE=1000,
                betaw1=-0.1,
                betaw2=-0.70,
-               betaw3=-1.2,betaw4=-1.2,Dq=0.8)
+               betaw3=-1.2,betaw4=-1.2,betawp1=-1,betawp2=-1,betawp3=-1,betawp4=-1.0,Dq=0.8)
 
 myOde<-LaunchMonolix(myOde, nameproject, obs, map,runToBeDone=FALSE, PopInitValue=PopInitValue )
 end_time<-Sys.time()
